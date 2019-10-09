94544327f9bfeb47642566a8036fe4228b7c6a20
Gustaf Hultgren <gustaf.hultgren@byon8.com>
2019-10-09 11:14:27 +0200

    remove image wrapper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
   <v-container>
-    <div class="img-wrapper">
-      <img src="XerpaDashboard.png" alt="hello" />
-    </div>
     <div class="text">
       <h1>This is a Hero message!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint mollitia accusantium obcaecati eum necessitatibus aut veniam, provident repellendus rem, tempora sit! Magni incidunt accusantium perspiciatis eaque culpa modi impedit dolores!</p>
